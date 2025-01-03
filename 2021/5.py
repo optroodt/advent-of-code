7707dc686c98dbd711c3b6c412ecc2a06110ae17
Youri op 't Roodt <y.optroodt@gmail.com>
2025-01-03 23:19:32 +0100

    Cleanup 2021

--- 2021/5.py
+++ 2021/5.py
@@ -1,69 +1,72 @@
+from utils import load_file
+
+lines = load_file("5.txt")
+
 coordinates = []
-max_x, max_y = 0,0
-with open('5_input.txt', 'r') as fh:
-	for line in fh.readlines():
 
-		a, b = line.strip().split(" -> ")
-		pair = [tuple(map(int, x.split(','))) for x in [a,b]]
-		coordinates.append(pair)
+for line in lines:
+    a, b = line.split(" -> ")
+    pair = [tuple(map(int, x.split(','))) for x in [a, b]]
+    coordinates.append(pair)
 
+max_x, max_y = 0, 0
 for a, b in coordinates:
-	max_x = max(max_x,a[0])
-	max_x = max(max_x,b[0])
-	max_y = max(max_y,a[1])
-	max_y = max(max_y,b[1])
+    max_x = max(max_x, a[0])
+    max_x = max(max_x, b[0])
+    max_y = max(max_y, a[1])
+    max_y = max(max_y, b[1])
 
-# print(coordinates, len(coordinates))
-print(max_x, max_y)
-grid = [0] * (max_x * max_y)
+
 def get_straight(values):
-	result = []
-	for a, b in values:
-		if a[0] == b[0] or a[1] == b[1]:
-			result.append([a,b])
-	return result
+    result = []
+    for a, b in values:
+        if a[0] == b[0] or a[1] == b[1]:
+            result.append([a, b])
+    return result
+
 
 straight = get_straight(coordinates)
-print(len(straight))
+
 
 def calculate_index(x, y):
-	return x + y*(max_x-1)
+    return x + y * (max_x - 1)
+
 
 def calculate_positions(a, b):
-	x_correction = -1 if b[0] < a[0] else 1
-	y_correction = -1 if b[1] < a[1] else 1
-	xs = range(a[0], b[0] + x_correction, x_correction)
-	ys = range(a[1], b[1] + y_correction, y_correction)
-	# print(xs, ys)
-	positions = []
+    x_correction = -1 if b[0] < a[0] else 1
+    y_correction = -1 if b[1] < a[1] else 1
+    xs = range(a[0], b[0] + x_correction, x_correction)
+    ys = range(a[1], b[1] + y_correction, y_correction)
+    # print(xs, ys)
+    positions = []
 
-	if a[0] != b[0] and a[1] != b[1]:
-		for x,y in zip(xs, ys):
-			positions.append((x,y))
-	else:
-		for x in xs:
-			for y in ys:
-				positions.append((x,y))
+    if a[0] != b[0] and a[1] != b[1]:
+        for x, y in zip(xs, ys):
+            positions.append((x, y))
+    else:
+        for x in xs:
+            for y in ys:
+                positions.append((x, y))
 
-	return positions
+    return positions
+
 
 def calculate_indices(positions):
+    return [calculate_index(x, y) for x, y in positions]
 
-	return [calculate_index(x,y) for x,y in positions]
 
-# for a,b in straight:
-for a,b in coordinates:
-	# print(a,b)
-	positions = calculate_positions(a,b)
-	
-	indices = calculate_indices(positions)
-	for index in indices:
-		# print(index, len(grid))
-		grid[index] += 1
+for title, coords in [("Part One", straight), ("Part Two", coordinates)]:
+    grid = [0] * (max_x * max_y)
+    for a, b in coords:
+        positions = calculate_positions(a, b)
 
-count = 0
-for i in grid:
-	if i > 1:
-		count += 1
+        indices = calculate_indices(positions)
+        for index in indices:
+            grid[index] += 1
 
-print(count)
+    count = 0
+    for i in grid:
+        if i > 1:
+            count += 1
+
+    print(f"{title}:", count)
--- 2021/6.py
+++ 2021/6.py
@@ -1,83 +1,66 @@
-import collections 
-with open('6_input.txt', 'r') as fh:
-	for line in fh.readlines():
-		states = [int(x) for x in line.strip().split(',')]
+import collections
+
+from utils import load_file
+
+lines = load_file("6.txt")
+
+for line in lines:
+    states = [int(x) for x in line.split(",")]
 
 
 class Fish:
+    def __init__(self, timer=8):
+        self.timer = timer
 
-	def __init__(self, timer=8):
-		self.timer = timer
+    def __repr__(self):
+        return f"<{self.__class__.__name__} timer={self.timer}>"
 
-	def __repr__(self):
-		return f"<{self.__class__.__name__} timer={self.timer}>"
+    def reset(self):
+        self.timer = 7
 
-	def reset(self):
-		self.timer = 7
+    def pass_day(self):
+        if self.timer == 0:
+            self.timer = 6
+            return Fish()
+        else:
+            self.timer -= 1
 
-	def pass_day(self):
-		if self.timer == 0:
-			self.timer = 6
-			return Fish()
-		else:
-			self.timer -= 1
 
 fishes = [Fish(timer=x) for x in states]
-# print(fishes)
-
 c = collections.Counter([f.timer for f in fishes])
-print(c)
 
 
+def count_fishes(days):
+    timeline = [0] * days
+    fish_count = len(fishes)
+    for k, count in c.items():
+        timeline[k] += count
 
-print(351188)
-days = 256
-initial_timeline= [0] * days
-# for k, count in c.items():
-# 	for d in range(k+6, days, 6):
-# 		initial_timeline[d] += count
+        for i in range(k + 7, days, 7):
+            timeline[i] += count
 
-timeline= [0] * days
-fish_count = len(fishes)
-for k, count in c.items():
-	timeline[k] += count
+    for d in range(0, days):
+        new_fishes = timeline[d]
+        fish_count += new_fishes
 
-	for i in range(k+7, days, 7):
-	# 	# print(i)
-		timeline[i] += count
-# for k, count in c.items():
-# 	timeline[k-1] += count
-	# for i in range(k+1, days, 6):
-	# 	# print(i)
-		# timeline[i] += count
+        for i in range(d + 9, days, 7):
+            timeline[i] += new_fishes
 
-for d in range(0, days):
-	new_fishes = timeline[d]
-	fish_count += new_fishes
-	# fish_count += initial_timeline[d]
-	print(d, new_fishes, fish_count)
+    return fish_count
 
 
-	for i in range(d+9, days, 7):
-		timeline[i] += new_fishes
+print("Part One:", count_fishes(80))  # correct: 351188
+print("Part Two:", count_fishes(256))  # correct: 1595779846729
 
-print(timeline)
-# print(sum(timeline[0:6]))
-print('BOOOM')
-print(fish_count) # correct: 1595779846729
-print('BOOOM')
-print(351188) # 80 days
+# below version is too slow
 # days = 80
-days = 80
-
-for day in range(days):
-	new_fishes = []
-	for fish in fishes:
-		result = fish.pass_day()
-		if result is not None:
-			new_fishes.append(result)
-	if new_fishes:
-		fishes.extend(new_fishes)
-
-	print(day, len(new_fishes), len(fishes))
-
+# for day in range(days):
+#     new_fishes = []
+#     for fish in fishes:
+#         result = fish.pass_day()
+#         if result is not None:
+#             new_fishes.append(result)
+#     if new_fishes:
+#         fishes.extend(new_fishes)
+#
+#     print(day, len(new_fishes), len(fishes))
