06f858e205604a5361ed06629a259ece794de5ca
Youri op 't Roodt <y.optroodt@gmail.com>
2025-01-05 18:42:49 +0100

    Complete Day 11, part two

--- 2024/11.py
+++ 2024/11.py
@@ -1,15 +1,22 @@
-import collections
-import copy
-import functools
 import itertools
-import operator
-import pathlib
-import re
-import string
 
 from utils import load_file
 
 lines = load_file()
+
+
+class Cache:
+    def __init__(self):
+        self.lookup = {}
+
+    def get(self, v, number_of_blinks):
+        return self.lookup.get((v, number_of_blinks))
+
+    def set(self, v, number_of_blinks, value):
+        self.lookup[(v, number_of_blinks)] = value
+
+
+cache = Cache()
 
 
 class Stone:
@@ -30,6 +37,19 @@ class Stone:
             return [Stone(self.value * 2024)]
 
 
+def recurse(stones, blinks, current):
+    if blinks == current:
+        return len(stones)
+    result = 0
+    for stone in stones:
+        value = cache.get(stone.value, current + 1)
+        if value is None:
+            value = recurse(stone.blink(), blinks, current + 1)
+            cache.set(stone.value, current + 1, value)
+        result += value
+    return result
+
+
 def run():
     starting_stones = [Stone(v) for v in lines[0].split(" ")]
     print(starting_stones)
@@ -38,7 +58,10 @@ def run():
     for i in range(number_of_blinks):
         stones = itertools.chain.from_iterable([stone.blink() for stone in stones])
     list_stones = list(stones)
-    print("Part One:", len(list_stones))  # , list_stones)
+    print("Part One:", len(list_stones))  # Correct answer: 190865
+
+    the_answer = recurse(starting_stones, 75, 0)
+    print("Part Two:", the_answer)  # Correct answer: 225404711855335
 
 
 if __name__ == "__main__":
